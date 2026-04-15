c10f4667f6d24aedc22fc153e4051c530251dd4b
ATR1095 <parul.mishra@gmail.com>
2026-04-15 15:21:26 +0530

    feature: fixed RPC error

--- decoder.go
+++ decoder.go
@@ -96,101 +96,160 @@ func (d *Decoder) Decode(poolAddr, programID string, data []byte) (*PoolData, er
 }
 
 // DecodePoolUpdate identifies the DEX and decodes the raw byte data
+// func DecodePoolUpdate(programID string, data []byte, slot uint64, poolAddr string) (*PoolData, error) {
+// 	switch programID {
+// 	case RaydiumProgramID, RaydiumProgramID2:
+// 		// Raydium V4 AMM accounts are 752 bytes of fixed layout.
+// 		if len(data) < 752 {
+// 			return nil, fmt.Errorf("raydium data too short: %d < 752", len(data))
+// 		}
+
+
+// 		// Raydium V4 Offsets:
+// 		// baseMint: 400, quoteMint: 432
+// 		// baseVault: 448, quoteVault: 480
+// 		// lpSupply: 552
+		
+// 		lpSupply := binary.LittleEndian.Uint64(data[552:560])
+
+// 		tokenA := base58.Encode(data[400:432])
+// 		tokenB := base58.Encode(data[432:464])
+// 		vaultA := base58.Encode(data[448:480])
+// 		vaultB := base58.Encode(data[480:512])
+
+// 		// Reserves will be populated by vault balance monitoring (handleVaultUpdate)
+// 		reservesA := uint64(0)
+// 		reservesB := uint64(0)
+
+// 		return &PoolData{
+// 			Address:         poolAddr,
+// 			TokenA:          tokenA,
+// 			TokenB:          tokenB,
+// 			VaultA:          vaultA,
+// 			VaultB:          vaultB,
+// 			ReservesA:       reservesA,
+// 			ReservesB:       reservesB,
+// 			LpSupply:        lpSupply,
+// 			LastUpdatedSlot: slot,
+// 			DexType:         "raydium",
+// 		}, nil
+
+// 	case RaydiumCPMMProgramID:
+// 		// Raydium CPMM PoolState layout:
+// 		// mint_a: 72, mint_b: 104
+// 		// vault_a: 168, vault_b: 200
+// 		if len(data) < 232 {
+// 			return nil, fmt.Errorf("raydium cpmm data too short: %d < 232", len(data))
+// 		}
+
+// 		tokenA := base58.Encode(data[168:200])
+// 		tokenB := base58.Encode(data[200:232])
+// 		vaultA := base58.Encode(data[72:104])
+// 		vaultB := base58.Encode(data[104:136])
+
+// 		return &PoolData{
+// 			Address:         poolAddr,
+// 			TokenA:          tokenA,
+// 			TokenB:          tokenB,
+// 			VaultA:          vaultA,
+// 			VaultB:          vaultB,
+// 			ReservesA:       0,
+// 			ReservesB:       0,
+// 			LpSupply:        0, 
+// 			LastUpdatedSlot: slot,
+// 			DexType:         "raydium_cpmm",
+// 		}, nil
+
+// 	case OrcaProgramID:
+// 		// Whirlpool account layout:
+// 		// sqrtPrice (u128) at offset 32
+// 		// liquidity (u128) at offset 16
+// 		// tokenVaultA (32 bytes) at offset 101
+// 		// tokenVaultB (32 bytes) at offset 133
+// 		if len(data) < 165 {
+// 			return nil, fmt.Errorf("orca whirlpool data too short: %d < 165", len(data))
+// 		}
+		
+// 		// Reading lower 64 bits for now, though technically u128
+// 		liquidity := binary.LittleEndian.Uint64(data[16:24]) 
+// 		// For CLMM, sqrtPrice is crucial. We read the first 8 bytes.
+// 		// Note: Proper CLMM support would require the full u128.
+// 		sqrtPrice := binary.LittleEndian.Uint64(data[32:40])
+		
+// 		vaultA := base58.Encode(data[101:133])
+// 		vaultB := base58.Encode(data[133:165])
+
+// 		return &PoolData{
+// 			Address:         poolAddr,
+// 			VaultA:          vaultA,
+// 			VaultB:          vaultB,
+// 			ReservesA:       liquidity,
+// 			ReservesB:       sqrtPrice,
+// 			LpSupply:        0, 
+// 			LastUpdatedSlot: slot,
+// 			DexType:         "orca",
+// 		}, nil
+
+// 	default:
+// 		return nil, fmt.Errorf("unsupported program ID: %s", programID)
+// 	}
+// }
+
 func DecodePoolUpdate(programID string, data []byte, slot uint64, poolAddr string) (*PoolData, error) {
-	switch programID {
-	case RaydiumProgramID, RaydiumProgramID2:
-		// Raydium V4 AMM accounts are 752 bytes of fixed layout.
-		if len(data) < 752 {
-			return nil, fmt.Errorf("raydium data too short: %d < 752", len(data))
-		}
+    switch programID {
+    case RaydiumProgramID, RaydiumProgramID2:
+        if len(data) < 752 { return nil, fmt.Errorf("data too short") }
 
+        return &PoolData{
+            Address:   poolAddr,
+            TokenA:    base58.Encode(data[400:432]), // baseMint
+            TokenB:    base58.Encode(data[432:464]), // quoteMint
+            VaultA:    base58.Encode(data[336:368]), // baseVault
+            VaultB:    base58.Encode(data[368:400]), // quoteVault
+            LpSupply:  binary.LittleEndian.Uint64(data[552:560]),
+            DexType:   "raydium",
+        }, nil
 
-		// Raydium V4 Offsets:
-		// baseMint: 400, quoteMint: 432
-		// baseVault: 448, quoteVault: 480
-		// lpSupply: 552
-		
-		lpSupply := binary.LittleEndian.Uint64(data[552:560])
+case RaydiumCPMMProgramID:
+    if len(data) < 232 {
+        return nil, fmt.Errorf("cpmm data too short")
+    }
 
-		tokenA := base58.Encode(data[400:432])
-		tokenB := base58.Encode(data[432:464])
-		vaultA := base58.Encode(data[448:480])
-		vaultB := base58.Encode(data[480:512])
+    // CORRECT CPMM OFFSETS:
+    // Mints are at 72 and 104
+    // Vaults are at 168 and 200
+    tokenA := base58.Encode(data[72:104])   // This MUST be the Mint
+    tokenB := base58.Encode(data[104:136])  // This MUST be the Mint
+    vaultA := base58.Encode(data[168:200])  // This is the Vault
+    vaultB := base58.Encode(data[200:232])  // This is the Vault
 
-		// Reserves will be populated by vault balance monitoring (handleVaultUpdate)
-		reservesA := uint64(0)
-		reservesB := uint64(0)
+    return &PoolData{
+        Address:   poolAddr,
+        TokenA:    tokenA,
+        TokenB:    tokenB,
+        VaultA:    vaultA,
+        VaultB:    vaultB,
+        DexType:   "raydium_cpmm",
+    }, nil
 
-		return &PoolData{
-			Address:         poolAddr,
-			TokenA:          tokenA,
-			TokenB:          tokenB,
-			VaultA:          vaultA,
-			VaultB:          vaultB,
-			ReservesA:       reservesA,
-			ReservesB:       reservesB,
-			LpSupply:        lpSupply,
-			LastUpdatedSlot: slot,
-			DexType:         "raydium",
-		}, nil
-
-	case RaydiumCPMMProgramID:
-		// Raydium CPMM PoolState layout:
-		// mint_a: 72, mint_b: 104
-		// vault_a: 168, vault_b: 200
-		if len(data) < 232 {
-			return nil, fmt.Errorf("raydium cpmm data too short: %d < 232", len(data))
-		}
-
-		tokenA := base58.Encode(data[168:200])
-		tokenB := base58.Encode(data[200:232])
-		vaultA := base58.Encode(data[72:104])
-		vaultB := base58.Encode(data[104:136])
-
-		return &PoolData{
-			Address:         poolAddr,
-			TokenA:          tokenA,
-			TokenB:          tokenB,
-			VaultA:          vaultA,
-			VaultB:          vaultB,
-			ReservesA:       0,
-			ReservesB:       0,
-			LpSupply:        0, 
-			LastUpdatedSlot: slot,
-			DexType:         "raydium_cpmm",
-		}, nil
-
-	case OrcaProgramID:
-		// Whirlpool account layout:
-		// sqrtPrice (u128) at offset 32
-		// liquidity (u128) at offset 16
-		// tokenVaultA (32 bytes) at offset 101
-		// tokenVaultB (32 bytes) at offset 133
-		if len(data) < 165 {
-			return nil, fmt.Errorf("orca whirlpool data too short: %d < 165", len(data))
-		}
-		
-		// Reading lower 64 bits for now, though technically u128
-		liquidity := binary.LittleEndian.Uint64(data[16:24]) 
-		// For CLMM, sqrtPrice is crucial. We read the first 8 bytes.
-		// Note: Proper CLMM support would require the full u128.
-		sqrtPrice := binary.LittleEndian.Uint64(data[32:40])
-		
-		vaultA := base58.Encode(data[101:133])
-		vaultB := base58.Encode(data[133:165])
-
-		return &PoolData{
-			Address:         poolAddr,
-			VaultA:          vaultA,
-			VaultB:          vaultB,
-			ReservesA:       liquidity,
-			ReservesB:       sqrtPrice,
-			LpSupply:        0, 
-			LastUpdatedSlot: slot,
-			DexType:         "orca",
-		}, nil
-
-	default:
-		return nil, fmt.Errorf("unsupported program ID: %s", programID)
-	}
+case OrcaProgramID:
+    if len(data) < 165 {
+        return nil, fmt.Errorf("orca data too short")
+    }
+    
+    // CORRECT ORCA WHIRLPOOL OFFSETS:
+    // Mints are at 101 and 133
+    tokenA := base58.Encode(data[101:133]) 
+    tokenB := base58.Encode(data[133:165])
+    
+    // Note: Vaults for Orca are not easily found at fixed offsets 
+    // in the pool state; they are usually separate metadata.
+    return &PoolData{
+        Address: poolAddr,
+        TokenA:  tokenA,
+        TokenB:  tokenB,
+        DexType: "orca",
+    }, nil
+    }
+    return nil, fmt.Errorf("unsupported program")
 }
